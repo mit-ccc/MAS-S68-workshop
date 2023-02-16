268aa1c69aaeae0ee71f5c438b9698310c50f822
Doug Beeferman <dougb@Dougs-MacBook-Air.local>
2023-02-16 14:33:23 -0500

    Add streamlit, move config to runtime args

--- reverse_dictionary.py
+++ reverse_dictionary.py
@@ -9,24 +9,20 @@ Prompts and evaluation data are in data/train.jsonl and data/test.jsonl respecti
 and were produced by ./pull_rd_data.py
 """
 
+import argparse
 import json
 import os
-import openai
 import re
 
-# The number of examples from data/train.jsonl to include in the prompt.  If 0, use a separate 0-shot prompt
-_NUM_FEW_SHOT_EXAMPLES = 5
-
-# The OpenAI model to use
-_MODEL = "text-curie-001"
-# _MODEL = "text-davinci-003"  # Uncomment to use the highest-powered model
+import openai
+import streamlit as st
 
 # Don't forget to set your OPENAI_API_KEY environment variable.
 # Or set it here directly (but don't check it into a git repo.)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def call_gpt3_api(prompt, model="text-davinci-003"):
+def call_gpt3_api(prompt, model):
     response = openai.Completion.create(
         model=model,
         prompt=prompt,
@@ -62,25 +58,24 @@ def response_to_completion_text(openai_response):
     return openai_response["choices"][0]["text"]
 
 
-def completion_text_to_first_word(s):
+def completion_text_to_words(s):
     words = re.sub("[^A-Za-z]", " ", s.strip()).split()
     if words:
-        return words[0]
+        return words
     else:
         # Return empty string if it produces no answer
-        return ""
+        return []
 
 
-def get_best_word_for_definition(definition, examples_to_use=None):
+def get_words_for_definition(definition, examples_to_use=None):
     if not examples_to_use:
         prompt = definition_to_zero_shot_prompt(definition)
     else:
         prompt = definition_to_few_shot_prompt(definition, examples_to_use)
 
-    openai_response = call_gpt3_api(prompt, model=_MODEL)
+    openai_response = call_gpt3_api(prompt, args.model)
     completion = response_to_completion_text(openai_response)
-    first_word = completion_text_to_first_word(completion)
-    return first_word
+    return completion_text_to_words(completion)
 
 
 def read_batch_of_queries(filename):
@@ -89,12 +84,12 @@ def read_batch_of_queries(filename):
 
 
 def get_example_queries_for_prompt(filename):
-    return read_batch_of_queries(filename)[:_NUM_FEW_SHOT_EXAMPLES]
+    return read_batch_of_queries(filename)[: args.num_prompt_examples]
 
 
 def run_batch_of_queries(evaluation_queries_filename, prompt_example_queries_filename):
     evaluation_queries = read_batch_of_queries(evaluation_queries_filename)
-    if _NUM_FEW_SHOT_EXAMPLES > 0:
+    if args.num_prompt_examples > 0:
         prompt_example_queries = get_example_queries_for_prompt(
             prompt_example_queries_filename
         )
@@ -103,11 +98,12 @@ def run_batch_of_queries(evaluation_queries_filename, prompt_example_queries_fil
     num_correct = 0
     for record in evaluation_queries:
         definition = record["definition"]
-        record["gpt3_best_word"] = get_best_word_for_definition(
+        record["gpt3_words"] = get_words_for_definition(
             definition, examples_to_use=prompt_example_queries
         )
         record["gpt3_is_correct"] = (
-            record["gpt3_best_word"].lower() == record["word"].lower()
+            len(record["gpt3_words"]) > 0
+            and record["gpt3_words"][0].lower() == record["word"].lower()
         )
         if record["gpt3_is_correct"]:
             num_correct += 1
@@ -118,6 +114,42 @@ def run_batch_of_queries(evaluation_queries_filename, prompt_example_queries_fil
     )
 
 
+def streamlit_app():
+    with st.form("main_form"):
+        query = st.text_input("Enter description of the word you're looking for")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write(get_words_for_definition(query))
+
+
 if __name__ == "__main__":
-    #    print(get_best_word_for_definition("clothing that you wear on your head"))   # uncomment to run a single query
-    run_batch_of_queries("data/test.jsonl", "data/train.jsonl")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--query", help="Look up words matching this description", default=None
+    )
+    parser.add_argument(
+        "--eval",
+        help="Run an evaluation on the given file of queries (in jsonl format)",
+        default=None,
+    )
+    parser.add_argument(
+        "--model", help="Which OpenAI model to use", default="text-curie-001"
+    )
+    parser.add_argument(
+        "--num_prompt_examples",
+        help="The number of examples from data/train.jsonl to include in the prompt.  If 0, use a separate 0-shot prompt.",
+        default=0,
+    )
+    args = parser.parse_args()
+
+    args.num_prompt_examples = int(args.num_prompt_examples)
+    if args.query:
+        print(
+            get_words_for_definition(
+                args.query, get_example_queries_for_prompt("data/train.jsonl")
+            )
+        )
+    elif args.eval:
+        run_batch_of_queries(args.eval, "data/train.jsonl")
+    else:
+        streamlit_app()
